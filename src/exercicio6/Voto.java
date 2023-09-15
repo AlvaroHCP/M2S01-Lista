f3021517e16c645e54234697ba53bd28aa4aed3e
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-09-15 17:39:39 -0300

    Função voto obrigatório ou não.

---
+++ src/exercicio6/Voto.java
@@ -0,0 +1,25 @@
+package exercicio6;
+
+import java.util.Scanner;
+
+public class Voto {
+    public static void main(String[] args) {
+        int idade;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a sua idade: ");
+        idade = Integer.parseInt(input.nextLine());
+
+        String votoProibido = "Você não pode votar.";
+        String votoOpcional = "Seu voto é opcional.";
+        String votoObrigatorio = "Seu voto é obrigatório.";
+
+        if(idade < 16){
+            System.out.println(votoProibido);
+        } else if (idade == 16 || idade == 17 || idade > 59){
+            System.out.println(votoOpcional);
+        } else {
+            System.out.println(votoObrigatorio);
+        }
+    }
+}
